4050d75e24cd3321484da497e2c1f2f07bafcb97
Simon Lynch <simon.lynch@hashicorp.com>
2025-07-24 10:09:53 +1000

    refactor: Update Vault AppRole configuration and remove unused variables

--- adk/vault_approle/auth.tf
+++ adk/vault_approle/auth.tf
@@ -1,5 +1,11 @@
+resource "vault_auth_backend" "approle" {
+  type = "approle"
+  path = "approle"
+}
+
+
 resource "vault_approle_auth_backend_role" "this" {
-  backend         = var.auth_backend_approle_path
+  backend         = vault_auth_backend.approle.path
   role_name       = var.tenant
   token_policies  = ["watsondemo"]
 }
@@ -19,6 +25,6 @@ resource "vault_identity_entity" "this" {
 #create entity alias for the role
 resource "vault_identity_entity_alias" "this" {
   name         = vault_approle_auth_backend_role.this.role_id
-  mount_accessor = var.approle_mount_accessor
+  mount_accessor = vault_auth_backend.approle.accessor
   canonical_id = vault_identity_entity.this.id
 }
